27191d5e1f882a5ae787f4092d972180d3a61a99
ErrorMaster <axallami@gmail.com>
2021-10-17 16:38:36 -0400

    Add temp player controller and scene

---
+++ The Nine Tales/Assets/_TheNineTales/Personal/Abdullah/Scripts/InputTracker.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public static class InputTracker 
+{
+    public const string AXIS_HORIZONTAL = "Horizontal";
+    public const string AXIS_VERTICAL = "Vertical";
+    public const string BUTTON_JUMP = "Jump";
+
+
+    public static Vector2 GetDirectionalInput()
+    {
+        return new Vector2(Input.GetAxisRaw(AXIS_HORIZONTAL), Input.GetAxisRaw(AXIS_VERTICAL));
+    }
+
+
+    public static bool WasJumpPressed()
+    {
+        return Input.GetButtonDown(BUTTON_JUMP);
+    }
+
+
+    public static bool IsJumpPressed()
+    {
+        return Input.GetButton(BUTTON_JUMP);
+    }
+}
