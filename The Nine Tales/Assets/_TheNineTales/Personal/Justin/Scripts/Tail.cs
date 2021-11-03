1c662bcb0d32ccb015acf848bfaf2b215ca4ee16
jvrieling <32886119+jvrieling@users.noreply.github.com>
2021-11-03 19:17:31 -0400

    Add modifier to tail wiggle
    Added functionality for the tail wiggling to react to the player controller's velocity

--- The Nine Tales/Assets/_TheNineTales/Personal/Justin/Scripts/Tail.cs
+++ The Nine Tales/Assets/_TheNineTales/Personal/Justin/Scripts/Tail.cs
@@ -12,17 +12,21 @@ public class Tail : MonoBehaviour
     [Header("Wiggle Settings")]
     public float wiggleSpeed = 3;
     public float wiggleMagnitude = 10;
+    public float velocitySpeedModifier = 0.5f;
 
     [Header("References")]
     public Transform tailBase;
     public LineRenderer lineRend;
     public Transform targetDir;
     public SpriteRenderer sr;
+    public Player characterController;
 
     private int lastLength;
     private Vector3[] segments;
     private Vector3[] segmentV;
     private Quaternion startingRotation;
+    private float originalWiggleSpeed;
+    private float originalWiggleMagnitude;
 
     private void Start()
     {
@@ -30,6 +34,8 @@ public class Tail : MonoBehaviour
         segments = new Vector3[length];
         segmentV = new Vector3[length];
         startingRotation = transform.rotation;
+        originalWiggleSpeed = wiggleSpeed;
+        originalWiggleMagnitude = wiggleMagnitude;
     }
     private void Reset()
     {
@@ -41,6 +47,7 @@ public class Tail : MonoBehaviour
         if (tailBase == null) tailBase = transform;
         if (sr == null) sr = GetComponentInParent<SpriteRenderer>();
         if (targetDir == null) targetDir = transform.GetChild(0);
+        if (characterController == null) characterController = GetComponentInParent<Player>();
 
         if (lastLength != length)
         {
@@ -52,6 +59,11 @@ public class Tail : MonoBehaviour
     }
     private void Update()
     {
+        //Modify the tail wiggling based on the current velocity in the player controller.
+        wiggleSpeed = originalWiggleSpeed + Mathf.Abs(characterController.m_PlayerVelocity.x) * velocitySpeedModifier;
+        wiggleMagnitude = originalWiggleMagnitude - Mathf.Abs(characterController.m_PlayerVelocity.x) * velocitySpeedModifier;
+
+        //Wiggle the tail by moving the rotation in a sine wave.
         targetDir.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * wiggleSpeed) * wiggleMagnitude);
 
         //Flip the target direction if the sprite is flipped.
